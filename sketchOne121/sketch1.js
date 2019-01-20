c97914e4ed1d42f2fe4e539b978ce07feab05bd4
Lily Donaldson <lilykdonaldson@gmail.com>
2019-01-20 15:21:13 -0500

    Merge all Lesson repos, Fall 18

---
+++ sketchOne121/sketch1.js
@@ -0,0 +1,7 @@
+function setup(){
+    createCanvas(1000, 600);
+    background(47,25,96);
+}
+function draw(){
+	ellipse(100,200,30,30);
+}
---
+++ sketchTwo121/sketch2.js
@@ -0,0 +1,7 @@
+function setup(){
+    createCanvas(400, 350);
+    background(241, 213, 204);
+}
+function draw(){
+	ellipse(mouseX,mouseY,30,30);
+}
